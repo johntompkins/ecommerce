912f04b02feb5f7cccbda507b5dd208a96221766
John <john.t.tompkins@gmail.com>
2016-11-20 23:51:20 -0500

    Mod search

--- searchstore.php
+++ searchstore.php
@@ -38,41 +38,44 @@
     session_start();
 
     if($_SERVER["REQUEST_METHOD"] == "POST"){
-      $name = $_POST['pname'];
-      $type = $_POST['btype'];
-      $price = $_POST['price'];
-      $whereQuer= "SELECT * FROM `Product`";
-      if($name || $type || $price){
-        $whereQuer .= " WHERE ";
-        if($name){
-          $whereQuer .= "`pname` = '".$name."' AND ";
-        }
-        if($price){
-          $whereQuer.="`price` <= ".$price;
-        }
-        else{
-          $whereQuer.="`price` >= ".$price;
-        }
-        if($type != "0"){
-          $whereQuer.= " AND `ptype` = ".$type;
-        }
+
+            $name = $_POST['pname'];
+            $type = $_POST['btype'];
+            $price = $_POST['price'];
+            $whereQuer= "SELECT * FROM `Product`";
+            if($name || $type || $price){
+              $whereQuer .= " WHERE ";
+              if($name){
+                $whereQuer .= "`pname` = '".$name."' AND ";
+              }
+              if($price){
+                $whereQuer.="`price` <= ".$price;
+              }
+              else{
+                $whereQuer.="`price` >= ".$price;
+              }
+              if($type != "0"){
+                $whereQuer.= " AND `ptype` = ".$type;
+              }
 
 
-      }
-      $whereQuer .= ";";
-      if(mysqli_query($db, $whereQuer)){
-        $result = mysqli_query($db, $whereQuer);
-        echo "<br><table border = '0' style='float: left'><tr><th>Image</th><th>Name</th><th>Original Price</th><th> Discounted Price</th> <th>Quantity</th></tr>";
+            }
+            $whereQuer .= ";";
+            if(mysqli_query($db, $whereQuer)){
+              $result = mysqli_query($db, $whereQuer);
+              echo "<br><table border = '0' style='float: left'><tr><th>Image</th><th>Name</th><th>Original Price</th><th> Discounted Price</th> <th>Quantity</th></tr>";
 
-        while($row = $result->fetch_assoc()){
-          $disPrice = $row['price'] * (1 - (.01 * $row['discount']));
-          echo "<tr> <td> <img class='formel' src ='http://lorempixel.com/100/100/''></td> <td>".$row['pname']."</td> <td>".$row['price']."</td> <td>".$disPrice."</td> <td>".$row['quantity']."</td> </tr>";
-        }
-        echo "</table>";
-      }
-      else{
-        echo "<br><p> Error in query!</p>";
-      }
+              while($row = $result->fetch_assoc()){
+                $disPrice = $row['price'] * (1 - (.01 * $row['discount']));
+                echo "<tr> <td> <img class='formel' src ='http://lorempixel.com/100/100/''></td> <td>".$row['pname']."</td> <td>".$row['price']."</td> <td>".$disPrice."</td> <td>".$row['quantity']."</td>
+                      <td><form method='POST' action = 'addtocart.php'><button class = 'btn btn-lg btn-primary btn-block' value='".$row['pid']."' type = 'submit' name = 'pid' >Add to Cart</button></form> </td> </tr>";
+              }
+              echo "</table>";
+            }
+            else{
+              echo "<br><p> Error in query!</p>";
+            }
+
 
     }
 
