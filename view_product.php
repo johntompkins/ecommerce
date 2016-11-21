67a85182dd1d1cab002cb8c5172a56e18be933ba
kash227 <kash227.netlab.uky.edu>
2016-11-20 20:20:19 -0500

    view product page

---
+++ view_product.php
@@ -0,0 +1,33 @@
+<?php
+
+$db = mysqli_connect('jtto224.netlab.uky.edu','root','dot.pan-79','Ecomm', 3306) or die('Error connecting to MySQL server.');
+
+
+// Check connection
+if ($conn->connect_error) {
+      die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "select * 
+	from product";
+echo $sql;
+echo "<br>";
+
+
+// perform the query
+echo "<br>";
+        $result = $conn->query($sql);
+         
+$row = $result->fetch_assoc();
+
+           echo "<tr>";
+           echo "<td>" . $row["pName"] . "</td>";
+           echo "<td>" . $row["ptype"] . "</td>";
+           echo "<td>" . $row["price"] . "</td>";
+           echo "<td>" . $row["discount"] . "</td>";
+           echo "<td>" . $row["quantity"] . "</td>";
+          
+           echo "</tr>";
+        
+	$conn->close();
+?>
