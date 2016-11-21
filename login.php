71797bad9f43b92aa22c099ca3da61a9244db487
kash227 <kash227.netlab.uky.edu>
2016-11-20 20:55:44 -0500

    login

--- login.php
+++ login.php
@@ -13,10 +13,12 @@
               $sql = "SELECT `sid` FROM `staff` WHERE `sid`='".$currusername."' and `pw`='".$currpass."';";
 		
 	      // first check if the query from the customer table works, if not try the staff table. If both fail, then invalid
-	      if(mysqil_query($db, $sql))
-	      { $result = mysqli_query($db, $sql);}
+	      if(mysqli_query($db, $sql))
+	      { $result = mysqli_query($db, $sql);
+	      }
 	      else
-	      { $result = mysqli_query($db, $sql1);}
+	      { $result = mysqli_query($db, $sql1);	
+	      }
 
               $row = mysqli_fetch_array($result, MYSQLI_ASSOC);
               $count = mysqli_num_rows($result);
