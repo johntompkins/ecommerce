31caa2b08bc039268918ffb8912b3303febbfdc2
kash227 <kash227.netlab.uky.edu>
2016-11-20 22:21:45 -0500

    land staff links

--- landing_staff.php
+++ landing_staff.php
@@ -11,4 +11,22 @@
       <h2><a href = "logout.php">Sign Out</a></h2>
    </body>
 
+      <h4> Employee's have ability to :</h4>
+    <form method="link" ACTION="staff_search_store.php">
+      <input type="submit" VALUE="Check Inventory">
+      </form>
+
+	<form method="link" ACTION="staff_restock_store.php">
+      <input type="submit" VALUE="Restock store">
+      </form>
+
+        <form method="link" ACTION="all_orders.php">
+      <input type="submit" VALUE="View all orders">
+      </form>
+
+	   <form method="link" ACTION="staff_ship_orders.php">
+      <input type="submit" VALUE="Ship orders to customers">
+      </form>
+
+
 </html>
