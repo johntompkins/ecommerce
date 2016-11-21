fec0dc888b8bb2cc9a70c63e88436b39b76be924
kash227 <kash227.netlab.uky.edu>
2016-11-20 20:30:59 -0500

    syntax

--- view_product.php
+++ view_product.php
@@ -3,20 +3,12 @@
 $db = mysqli_connect('jtto224.netlab.uky.edu','root','dot.pan-79','Ecomm', 3306) or die('Error connecting to MySQL server.');
 
 
-// Check connection
-if ($conn->connect_error) {
-      die("Connection failed: " . $conn->connect_error);
-}
-
 $sql = "select * 
 	from `Product`;";
-echo $sql;
-echo "<br>";
-
 
 // perform the query
 echo "<br>";
-        $result = $conn->query($sql);
+        $result = $mysqli_query($db, $sql);
          
 $row = $result->fetch_assoc();
 
