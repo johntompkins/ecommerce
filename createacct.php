c302d980f867dde55c85df5cb4219cd40f8b9241
kash227 <kash227.netlab.uky.edu>
2016-11-20 19:17:28 -0500

    added staff insert, need condition for type

--- createacct.php
+++ createacct.php
@@ -5,12 +5,12 @@ $db = mysqli_connect('jtto224.netlab.uky.edu','root','dot.pan-79','Ecomm', 3306)
 <body>
   <form method="POST">
     <h4> Create Username/password:</h4>
-    Username: <input type="text" name="username"><br>
+    Username: <input type="text" name="cid"><br>
     Password: <input type="password"  name="password" required><br>
     Full  name: <input type = "text" name="name"> <br>
     Address: <input type = "text" name="address"> <br>
     Payment: <input type = "text" name="payment"> <br>
-    Type: <select>
+    Type: <select name="typeSelect">
     <option value="customer">Customer</option>
     <option value="staff">Staff</option>
     <option value="manager">Manager</option>
@@ -20,8 +20,10 @@ $db = mysqli_connect('jtto224.netlab.uky.edu','root','dot.pan-79','Ecomm', 3306)
   </form>
  <?php
       if($_SERVER["REQUEST_METHOD"] == "POST"){
-          $sql = "INSERT INTO `customer` (`cid`, `password`, `name`, `address`, `payment`) VALUES ('".$_POST['cid']."', '".$_POST['password']."', '".$POST['name']."', '".$POST['address']."', '".$POST['payment']."');";
+          $sql = "INSERT INTO `customer` (`cid`, `password`, `name`, `address`, `payment`, `type`) VALUES ('".$_POST['cid']."', '".$_POST['password']."', '".$_POST['name']."', '".$_POST['address']."', '".$_POST['payment']."', '".$_POST['typeSelect']."');";
           $result = mysqli_query($db, $sql);
+
+	  $sql_staff = "INSERT INTO `staff` (`sid`, `pw`, `sType`) VALUES (;".$_POST['cid']."', '".$_POST['password']."', '".$_POST['typeSelect']."');";
 
           $count = mysqli_num_rows($result);
           if($result){
