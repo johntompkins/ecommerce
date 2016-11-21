5b36b05d85cfbcb6d5768cbe38a8288c4709cb19
kash227 <kash227.netlab.uky.edu>
2016-11-20 19:04:57 -0500

    added more inputs to create account

--- createacct.php
+++ createacct.php
@@ -5,16 +5,22 @@ $db = mysqli_connect('jtto224.netlab.uky.edu','root','dot.pan-79','Ecomm', 3306)
 <body>
   <form method="POST">
     <h4> Create Username/password:</h4>
-    <input type="text" name="username">
-    <input type="password"  name="password" required>
-    First name: <input type = "text" name="FirstName" ><br>
-    Last name: <input type = "text" name="LastName"> <br>
+    Username: <input type="text" name="username"><br>
+    Password: <input type="password"  name="password" required><br>
+    Full  name: <input type = "text" name="name"> <br>
+    Address: <input type = "text" name="address"> <br>
+    Payment: <input type = "text" name="payment"> <br>
+    Type: <select>
+    <option value="customer">Customer</option>
+    <option value="staff">Staff</option>
+    <option value="manager">Manager</option>
+    </select>
     <button class = "btn btn-lg btn-primary btn-block" type = "submit"
              name = "login">Create</button>
   </form>
  <?php
       if($_SERVER["REQUEST_METHOD"] == "POST"){
-          $sql = "INSERT INTO `login` (`username`, `password`) VALUES ('".$_POST['username']."', '".$_POST['password']."');";
+          $sql = "INSERT INTO `customer` (`cid`, `password`, `name`, `address`, `payment`) VALUES ('".$_POST['cid']."', '".$_POST['password']."', '".$POST['name']."', '".$POST['address']."', '".$POST['payment']."');";
           $result = mysqli_query($db, $sql);
 
           $count = mysqli_num_rows($result);
