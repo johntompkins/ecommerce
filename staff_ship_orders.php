f56f12e202a83593eeb41f9ea865c9e13d4f0a8a
kash227 <kash227.netlab.uky.edu>
2016-11-25 10:55:26 -0500

    manager stuff

---
+++ staff_ship_orders.php
@@ -0,0 +1,28 @@
+<?php
+    $db = mysqli_connect('jtto224.netlab.uky.edu','root','dot.pan-79','Ecomm', 3306) or die('Error connecting to MySQL server.');
+    session_start();
+     $whereQuer = "select i.`cid`, i.`pid`, o.`total`, o.`status`, i.`cartid` 
+FROM in_cart i, orders o, Product p
+WHERE o.`cartid`=i.`cartid`
+and o.`cid`=i.`cid` 
+and p.`pid`=i.`pid`
+group by `cid`,`cartid`  ";
+            if(mysqli_query($db, $whereQuer)){
+              $result = mysqli_query($db, $whereQuer);
+              echo "
+<h2><a href ='landing_staff.php'>Return to staff options</a></h2>
+<br><table border = '0' style='float: left'><tr><th>Customer ID</th><th>Cart ID </th></th><th>Products</th><th> Total Price</th> <th>Status</th></tr>";
+              while($row = $result->fetch_assoc()){
+                //$disPrice = $row['price'] * (1 - (.01 * $row['discount']));
+                echo "<tr> <td> ".$row['cid']." <td> ".$row['cartid']."</td> <td>".$row['pid']."</td> <td>".$row['total']."</td> <td>".$row['status']."</td>
+                    
+		<td><form method= 'POST' action='update_status.php'> <button type='submit' name='status' value='".$row['cartid']."'>Ship It </button></form>
+ </td>
+   </tr>";
+              }
+              echo "</table> <br> ";
+            }
+            else{
+              echo "<br><p> Error in query!</p>";
+            }
+?>
