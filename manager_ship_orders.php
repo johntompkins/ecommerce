e38b1d5359bed6e8ebe46212bc708f26bb008ee7
kash227 <kash227.netlab.uky.edu>
2016-12-06 19:57:09 -0500

    manager ship orders view order button

--- manager_ship_orders.php
+++ manager_ship_orders.php
@@ -1,11 +1,12 @@
 
+
 <?php
     $db = mysqli_connect('jtto224.netlab.uky.edu','root','dot.pan-79','Ecomm', 3306) or die('Error connecting to MySQL server.');
     session_start();
-     $whereQuer = "select i.`cid`, i.`pid`, o.`total`, o.`status`, i.`cartid` 
+     $whereQuer = "select i.`cid`, i.`pid`, o.`total`, o.`status`, i.`cartid`
 FROM in_cart i, orders o, Product p
 WHERE o.`cartid`=i.`cartid`
-and o.`cid`=i.`cid` 
+and o.`cid`=i.`cid`
 and p.`pid`=i.`pid`
 group by `cid`,`cartid`  ";
             if(mysqli_query($db, $whereQuer)){
@@ -14,16 +15,20 @@ group by `cid`,`cartid`  ";
 <h2><a href ='landing_manager.php'>Return to manager options</a></h2>
 <br><table border = '0' style='float: left'><tr><th>Customer ID</th><th>Cart ID </th></th><th>Products</th><th> Total Price</th> <th>Status</th></tr>";
               while($row = $result->fetch_assoc()){
+
+//              if($row['status'] = 0){
+
                 //$disPrice = $row['price'] * (1 - (.01 * $row['discount']));
-                echo "<tr> <td> ".$row['cid']." <td> ".$row['cartid']."</td> <td>".$row['pid']."</td> <td>".$row['total']."</td> <td>".$row['status']."</td>
-                    
-		<td><form method= 'POST' action='update_status_manager.php'> <button type='submit' name='status' value='".$row['cartid']."'>Ship It </button></form>
+                echo "<tr> <td> ".$row['cid']." <td> ".$row['cartid']."</td><td> <form method='POST' action='vieworder_manager.php'> <button class = 'btn btn-lg btn-primary btn-block' value='".$row['cartid']."' type = 'submit' name = 'cart' > View Order</button> </form> </td> <td>".$row['total']."</td> <td>".$row['status']."</td>
+
+                <td><form method= 'POST' action='update_status.php'> <button type='submit' name='status' value='".$row['cartid']."'>Ship It </button></form>
  </td>
    </tr>";
-              }
+              }//}
               echo "</table> <br> ";
             }
             else{
               echo "<br><p> Error in query!</p>";
             }
 ?>
+
